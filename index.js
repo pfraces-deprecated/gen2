5a39567fde7b733ede11f3ec494fe82d5fcf85ae
Pau Fracés <pfraces@gmail.com>
2013-02-18 03:46:59 +0100

    refactor: defining frame

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ module.exports = function (config) {
 };
 
 var Gen = function (config) {
+  var self = this;
   this.actors = [];
-  this.render = render(config, this.actors);
+  this.render = render(extend(config, {
+    frame: function () {
+      frame(self.actors);
+    }
+  }));
 };
 
-Gen.prototype.actor = function (pos, color) {
+Gen.prototype.actor = function (pos) {
   var a = actor(pos, [{
-    el: this.render.tile(color),
+    el: this.render.tile(),
     x: 0,
     y: 0
   }]);
   this.actors.push(a);
   return a;
 };
+
+var frame = function (actors) {
+  actors.forEach(function (actor) {
+    actor.act();
+  });
+
+  actors.forEach(function (actor) {
+    actor.members.forEach(function (member) {
+      member.el.pos({
+        x: ((actor.x + member.x) * self.cell).toString() + 'px',
+        y: ((actor.y + member.y) * self.cell).toString() + 'px'
+      })
+    });
+  });
+}
